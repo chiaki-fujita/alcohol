eaaaa7369b2c87d37df3f7e5f05b07e2f76ce5a2
藤田千晶 <f.chiaki.0927@gmail.com>
2026-02-10 00:32:18 +0900

    Add slick slider configuration
    Initialize slick slider with responsive settings.

---
+++ slick.js
@@ -0,0 +1,29 @@
+
+$('.slider').slick({
+		autoplay: true,//自動的に動き出すか。初期値はfalse。
+		infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+		slidesToShow: 3,//スライドを画面に3枚見せる
+		slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+		arrows:false,// ← これで Next / Prev ボタンを非表示にする
+        centerMode: true,      // ★ 見切れ用
+        centerPadding: '60px', // ★ 見切れ幅（調整OK）
+
+
+
+		responsive: [
+			{
+			breakpoint: 769,//モニターの横幅が769px以下の見せ方
+			settings: {
+				slidesToShow: 2,//スライドを画面に2枚見せる
+				slidesToScroll: 2,//1回のスクロールで2枚の写真を移動して見せる
+			}
+		},
+		{
+			breakpoint: 426,//モニターの横幅が426px以下の見せ方
+			settings: {
+				slidesToShow: 1,//スライドを画面に1枚見せる
+				slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+			}
+		}
+	]
+	});
